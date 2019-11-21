e66c0eb914f9bfd9afc2135f165f753e267e83ec
Jhensen Agni <jagni001@ucr.edu>
2019-11-20 20:03:14 -0800

    added a portfolio section with in-class assignments and projects

---
+++ porfolioScript.js
@@ -0,0 +1,19 @@
+//TODO:
+    // Add Quiz to Porfolio
+    // Add passwordGenerator to portfolio
+    // add todo list to portfolio
+    // add calculator to porfolio, when complete
+
+
+// create a function that for builds a card for each App on the portfolio
+
+/*<div class="card portfolioObject" style="width: 18rem;" >
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body portfolioObject-inner">
+                    <h5 class="card-title" style="color:black;">Card title</h5>
+                    <p class="card-text" style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div> */
+
+
