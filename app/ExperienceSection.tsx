6bdf2a3ccc881033d6864560d6204cd40588c63d
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2024-12-28 04:02:22 -0800

    add resume button

--- app/ExperienceSection.tsx
+++ app/ExperienceSection.tsx
@@ -1,6 +1,9 @@
 /**
  * Experience Section
  */
+import Link from "next/link";
+import { FileUser } from "lucide-react";
+
 import ExperienceCard from "./ExperienceCard";
 import SectionHeader from "./SectionHeader";
 import { workExperienceList } from "./constants";
@@ -21,6 +24,18 @@ export default function ExperienceSection() {
           title={title}
         />
       ))}
+      <div>
+
+        <Link
+          className="text-teal border border-teal rounded-full p-2 hover:bg-teal hover:text-navy inline-flex"
+          href="final-june-24-resume.pdf"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <span className="mr-2">View Full Resume</span>
+          <FileUser />
+        </Link>
+      </div>
     </section>
   );
 }
