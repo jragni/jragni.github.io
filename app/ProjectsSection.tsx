8f09e25671d20cf44ef7a1f5a1a28bc778b20210
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2024-12-28 04:30:16 -0800

    add wip to projcec againt

--- app/ProjectsSection.tsx
+++ app/ProjectsSection.tsx
@@ -8,7 +8,7 @@ export default function ProjectsSection() {
   return (
     <section className="min-h-screen p-6" id="projects">
       <SectionHeader label="Projects" />
-      <h3 className="text-4xl">WIP</h3>
+      <h3 className="text-4xl">WIP...</h3>
     </section>
   );
 }
