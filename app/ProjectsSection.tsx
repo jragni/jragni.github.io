c05dea5851e03ebe64a3284b52e95b2f45b69e65
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2024-12-28 04:02:22 -0800

    add resume button

--- app/ProjectsSection.tsx
+++ app/ProjectsSection.tsx
@@ -2,9 +2,12 @@
  * Projects Section
  */
 
+import SectionHeader from "./SectionHeader";
+
 export default function ProjectsSection() {
   return (
     <section className="min-h-screen p-6" id="projects">
+      <SectionHeader label="Projects" />
     </section>
   );
 }
