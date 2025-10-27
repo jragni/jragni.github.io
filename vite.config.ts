ad74cc25ac764c6b2c69c001447463faa360ba50
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-10-27 01:12:43 -0700

    add config workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Since this is jragni.github.io (user site), base should be '/'
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
